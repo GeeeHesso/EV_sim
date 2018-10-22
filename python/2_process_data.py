4b66fdfb0ca2c5042b7d64d35a16b52eec8e6525
David T <david.tauxe@hevs.ch>
2018-10-22 11:50:34 +0200

    Added interpolation and convolution

--- python/2_process_data.py
+++ python/2_process_data.py
@@ -76,22 +76,29 @@ inter_d2 = inter_d2[~np.isnan(inter_d2)]
 
 data_x1=np.array(data[:,0])
 data_y1=np.array(data[:,1])
+data_z1=np.array(data[:,2])
 data_x1= data_x1[~np.isnan(data_x1)]
 data_y1= data_y1[~np.isnan(data_y1)]
+data_z1= data_z1[~np.isnan(data_z1)]
+
 
 data_x2=np.array(data[:,3])
 data_y2=np.array(data[:,4])
+data_z2=np.array(data[:,5])
 data_x2= data_x2[~np.isnan(data_x2)]
 data_y2= data_y2[~np.isnan(data_y2)]
+data_z2= data_z2[~np.isnan(data_z2)]
 
 dist1_end=np.linspace(0,inter_d1[-1],1000)
 dist2_end=np.linspace(0,inter_d2[-1],1000)
 
 inter_x1 = np.interp(dist1_end,inter_d1,data_x1)
 inter_y1 = np.interp(dist1_end,inter_d1,data_y1)
+inter_z1 = np.interp(dist1_end,inter_d1,data_z1)
 
 inter_x2 = np.interp(dist2_end,inter_d2,data_x2)
 inter_y2 = np.interp(dist2_end,inter_d2,data_y2)
+inter_z2 = np.interp(dist2_end,inter_d2,data_z2)
 
 
 
@@ -117,35 +124,91 @@ inter_y2 = np.interp(dist2_end,inter_d2,data_y2)
 
 
 #####################################################
+# Lissage
 #####################################################
 
-plt.plot(data[:,1],data[:,0],'x',color='r')
-plt.plot(data[:,4],data[:,3],color='b')
-plt.title("Position")
-plt.figure()
+N=9
+lissage=(1./N)*np.ones(N)
 
-plt.plot(distance_1,data[:,2],'x',color='r')
-plt.plot(distance_2,data[:,5],color='b')
-plt.title("Height")
+l_x1=np.convolve(lissage,inter_x1,'same')
+l_y1=np.convolve(lissage,inter_y1,'same')
+l_z1=np.convolve(lissage,inter_z1,'same')
+l_z1[0:4]=inter_z1[0:4]
+l_x1[0:4]=inter_x1[0:4]
+l_y1[0:4]=inter_y1[0:4]
+l_z1[-5:]=inter_z1[-5:]
+l_x1[-5:]=inter_x1[-5:]
+l_y1[-5:]=inter_y1[-5:]
 
-plt.figure()
-plt.plot(diff_1)
-plt.title("Slope")
-plt.figure()
-plt.plot(diff_2)
-plt.title("Slope")
-
-plt.figure()
-plt.plot(d_d1)
-plt.title("Diff distance")
-plt.figure()
-plt.plot(d_d2)
-plt.title("Diff distance")
-
-plt.figure()
-plt.plot(inter_y1,inter_x1,'x',color='r')
-plt.plot(inter_y2,inter_x2,color='b')
-plt.title("Interpolation")
+l_x2=np.convolve(lissage,inter_x2,'same')
+l_y2=np.convolve(lissage,inter_y2,'same')
+l_z2=np.convolve(lissage,inter_z2,'same')
+l_z2[0:4]=inter_z2[0:4]
+l_y2[0:4]=inter_y2[0:4]
+l_x2[0:4]=inter_x2[0:4]
+l_z2[-5:]=inter_z2[-5:]
+l_y2[-5:]=inter_y2[-5:]
+l_x2[-5:]=inter_x2[-5:]
 
 
-plt.show()
+
+#####################################################
+# Saving file
+#####################################################
+
+np.savetxt('Lisse1.csv',np.array([l_x1,l_y1,l_z1]).T,delimiter=',',newline='\n')
+np.savetxt('Lisse2.csv',np.array([l_x2,l_y2,l_z2]).T,delimiter=',',newline='\n')
+
+np.savetxt('Interp1.csv',np.array([inter_x1,inter_y1,inter_z1]).T,delimiter=',',newline='\n')
+np.savetxt('Interp2.csv',np.array([inter_x2,inter_y2,inter_z2]).T,delimiter=',',newline='\n')
+
+
+
+
+# #####################################################
+# #####################################################
+# plt.plot(data[:,1],data[:,0],'x',color='r')
+# plt.plot(data[:,4],data[:,3],color='b')
+# plt.title("Position")
+# plt.figure()
+
+# plt.plot(distance_1,data[:,2],'x',color='r')
+# plt.plot(distance_2,data[:,5],color='b')
+# plt.title("Height")
+
+# plt.figure()
+# plt.plot(diff_1)
+# plt.title("Slope")
+# plt.figure()
+# plt.plot(diff_2)
+# plt.title("Slope")
+
+# plt.figure()
+# plt.plot(d_d1)
+# plt.title("Diff distance")
+# plt.figure()
+# plt.plot(d_d2)
+# plt.title("Diff distance")
+
+# plt.figure()
+# plt.plot(inter_y1,inter_x1,'x',color='r')
+# plt.plot(inter_y2,inter_x2,color='b')
+# plt.title("Interpolation")
+
+# plt.figure()
+# plt.plot(dist1_end,inter_z1,'x',color='r') #Need to check the height...
+# plt.plot(dist2_end,inter_z2,color='b')
+# plt.title("Height")
+
+# plt.figure()
+# plt.plot(l_y1,l_x1,'x',color='r')
+# plt.plot(l_y2,l_x2,color='b')
+# plt.title("Lissage")
+
+# plt.figure()
+# plt.plot(dist1_end,l_z1,'x',color='r') #Need to check the height...
+# plt.plot(dist2_end,l_z2,color='b')
+# plt.title("l_Height")
+
+
+# plt.show()
