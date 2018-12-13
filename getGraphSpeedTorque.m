a1b153cec73ef2a47b14b245c0c5c4983d2b21be
sermo1920 <sermo1920@users.noreply.github.com>
2018-12-13 16:22:30 +0100

    MAJ (TorqueSpeed)

--- getGraphSpeedTorque.m
+++ getGraphSpeedTorque.m
@@ -1,38 +1,32 @@
 r =9.73;
 nr=0.9
+rr = 0.33
 
-coupleMax = 660;%*r*nr;
-seuilCoupleMin = -660;%*r*nr;
-vitesseDecrochage = 80;
-vitesseMax = 200;
-vitesseFreinageReg = 20;
+coupleMaxMoteur = 430; %Nm
+coupleMaxRoues = coupleMaxMoteur * nr * r; %Nm
+puissanceMaxMoteur = 222121; %W
+puissanceMaxRoues = puissanceMaxMoteur * nr; %W
 
-vitesse = 1:1:vitesseMax ;
+vitesseMax = 195/3.6;%m/s
+vitesseDecroRadS = puissanceMaxRoues / coupleMaxRoues;
+vitesseDecroMS = puissanceMaxRoues / coupleMaxRoues * rr;
 
-for k=1:1:length(vitesse)
+vitesseCouple = 0:1:vitesseMax
+
+for k=1:1:vitesseMax+1
     
-    if vitesse(k)<= vitesseDecrochage
-        couplePositif(k) = coupleMax
-    elseif vitesse(k)>vitesseDecrochage
-        couplePositif(k) = 1/k *coupleMax
+    if vitesseCouple(k) <= vitesseDecroMS
+        seuilCoupleMax(k) = coupleMaxRoues;
+    elseif vitesseCouple(k) >= vitesseDecroMS
+        seuilCoupleMax(k)= puissanceMaxRoues / (vitesseCouple(k)/rr)
     end
 end
-    
-    plot(vitesse, couplePositif)
-    
-    
-    % x = [0, vitesseDecrochage, vitesseMax, vitesseDecrochage, vitesseFreinageReg, 0];
-    % y = [seuilCoupleMax, seuilCoupleMax, 0, seuilCoupleMin, seuilCoupleMin, 0];
-    
-%     xp = [0, vitesseDecrochage, vitesseMax];
-%     yp = [seuilCoupleMax, seuilCoupleMax, 0];
-%     xn = [vitesseMax, vitesseDecrochage, vitesseFreinageReg, 0];
-%     yn = [0, seuilCoupleMin, seuilCoupleMin, 0];
-%     
-%     v=(1:1:vitesseMax);
-%     
-%     yp_int=interp1(xp,yp,v,'pchip');
-%     yn_int=interp1(xn,yn,v,'pchip');
-%     
-%     figure(5);
-%     plot(v,yp_int,'b',v,yn_int,'g');
+
+vitesseCouple = vitesseCouple *3.6;
+seuilCoupleMax = seuilCoupleMax /nr/r;
+
+seuilCoupleMin= (-1)*flipud(seuilCoupleMax);
+
+
+plot(vitesseCouple, seuilCoupleMax, vitesseCouple, seuilCoupleMin )
+grid ON
